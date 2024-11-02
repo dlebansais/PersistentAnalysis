14578911fa26a32e13a0ad8d3f97b6668dbf8726
David Le Bansais <dlebansais@gmail.com>
2024-11-02 17:44:48 +0100

    Added test project.

---
+++ Test/PersistentAnalysis.Test/TestAssert.cs
@@ -0,0 +1,14 @@
+﻿namespace PersistentAnalysis.Test;
+
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+[TestFixture]
+public class TestAssert
+{
+    [Test]
+    public void TestSuccess()
+    {
+    }
+}
